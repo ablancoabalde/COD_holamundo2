b743c8e2a4cd6cbbdf4f48b3dc6d6dde728d5a66
ablancoabalde <ablancoabalde@victoria6.danielcastelao.org>
2017-10-06 09:26:13 +0200

    1º Modificacion

--- src/cod_holamundo2/COD_HolaMundo2.java
+++ src/cod_holamundo2/COD_HolaMundo2.java
@@ -4,7 +4,9 @@ package cod_holamundo2;
 public class COD_HolaMundo2 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola mundo");
+        
+        System.out.println("ñeñeñeñeñe");
     }
     
 }
